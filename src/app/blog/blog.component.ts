de1b4f1d4e38740d2d759b5277960c7b1100d00e
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-16 23:06:04 +0530

    Blog component added.

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.css']
 })
-export class BlogComponent implements OnInit {
+export class BlogComponent implements AfterViewInit {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  mArticles:any;
+  constructor(private dataService:DataService) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    this.dataService
+      .getBlogData()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((res: HttpResponse<any>) => {
+        this.mArticles = res.body;
+    });
   }
-
 }
