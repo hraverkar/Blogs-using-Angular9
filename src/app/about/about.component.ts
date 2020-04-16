6a8c816cc7a1ca67e9a0253fb1174b023dd293ea
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-16 21:25:22 +0530

    About component updated.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
@@ -11,5 +17,11 @@ export class AboutComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
 
 }
