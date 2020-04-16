7b07d64784ebb20dd4c71f3188edb37797d62b48
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-16 21:24:49 +0530

    Contact component updated.

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
+import { DataService } from '../services/data.service';
+import { v4 as uuidv4 } from 'uuid';
+import { MatSnackBar, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact',
@@ -16,7 +19,16 @@ import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 export class ContactComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
-  constructor(private _formBuilder: FormBuilder) {}
+  thirdFormGroup: FormGroup;
+
+  name:string;
+  contact:string;
+  email:string;
+  address:string;
+
+  constructor(private _formBuilder: FormBuilder, private dataServices: DataService
+    ,private _snackBar: MatSnackBar,
+    @Inject(MAT_SNACK_BAR_DATA) public dataSnackBar: any) {}
 
   ngOnInit(): void {
     this.firstFormGroup = this._formBuilder.group({
@@ -24,6 +36,39 @@ export class ContactComponent implements OnInit {
     });
     this.secondFormGroup = this._formBuilder.group({
       secondCtrl: ['', Validators.required],
+      second1Ctrl:['', Validators.required]
     });
+    this.thirdFormGroup = this._formBuilder.group({
+      thirdCtrl: ['', Validators.required],
+    });
+  }
+
+  form3(){
+    let id = uuidv4();
+    this.email = this.secondFormGroup.value.second1Ctrl;
+    this.contact = this.secondFormGroup.value.secondCtrl;
+    this.name = this.firstFormGroup.value.firstCtrl;
+    this.address = this.thirdFormGroup.value.thirdCtrl;
+    console.log(this.thirdFormGroup.value);
+    this.dataServices.addUserInfo(id,this.name,this.contact, this.email,this.address).then((result)=>{
+      this._snackBar.open("User Added into database", null, this.dataSnackBar.duration);
+    })
+    .catch(err=>{
+      this._snackBar.open("User information not added", null,this.dataSnackBar.duration);
+      console.log('register user data (firestore)', err.message);
+    }).finally(()=>
+      //this.reset()
+      console.log("Harshal raveerkar")
+      );
+    }
+
+  reset(): void {
+    this.name=null;
+    this.contact = null;
+    this.email = null;
+    this.address = null;
+    this.firstFormGroup=null;
+    this.thirdFormGroup =null;
+    this.thirdFormGroup = null;
   }
 }
