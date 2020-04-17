0cacca82b76576e2414050b2cd3443449552e7c4
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-18 00:46:36 +0530

    App routing updated.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,29 +4,42 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { BlogComponent } from './blog/blog.component';
-
+import { LoginComponent } from './login/login.component';
+import { UploadComponent } from './upload/upload.component';
 
 const routes: Routes = [
   {
-    path:'home', component:HomeComponent
+    path: 'home',
+    component: HomeComponent,
   },
   {
-    path:'', component:HomeComponent
+    path: '',
+    component: HomeComponent,
   },
   {
-    path:'about', component:AboutComponent
+    path: 'about',
+    component: AboutComponent,
   },
   {
-    path:'contact', component:ContactComponent
+    path: 'contact',
+    component: ContactComponent,
   },
   {
-    path:'blog', component:BlogComponent
+    path: 'blog',
+    component: BlogComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'upload',
+    component: UploadComponent,
   }
-
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
