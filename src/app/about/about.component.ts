f19675e64303831d1e39969f4f2a7d3395804f05
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-17 19:16:43 +0530

    About updated.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -18,7 +18,7 @@ export class AboutComponent implements OnInit {
   ngOnInit(): void {
   }
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'One', cols: 2, rows: 1, color: 'lightblue'},
     {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
     {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
