bcf8faba4060fa911d7d46930c82449c17b16789
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-19 00:46:59 +0530

    Menu option updated.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
+import { MatSnackBar, MAT_SNACK_BAR_DATA } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-menu',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
-
-  constructor() { }
+  public isUserLogin : boolean = false;
+  constructor(private dataService : DataService, private router : Router,
+    private _snackBar: MatSnackBar,
+    @Inject(MAT_SNACK_BAR_DATA) public dataSnackBar: any) { }
 
   ngOnInit(): void {
   }
 
+  logout() {
+    this.dataService.logout()
+    .then(result => {
+      this.router.navigate(['/login']);
+      this._snackBar.open("User logged out", null, this.dataSnackBar.duration)
+    })
+    .catch(err => {
+      console.log('err', err.message);
+      this._snackBar.open("Some error occured", null,this.dataSnackBar.duration)
+
+    })
+  }
 }
