e069c9ec19a3cd708a277412c1d5e42c4007f603
Harshal Raverkar <harshal.raverkar@gmail.com>
2020-04-19 00:48:45 +0530

    Enviroment added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyCXRXalVt4i_WEFKcSJG2ETebC9RXn12qc",
+  authDomain: "harshalblogs-52e5f.firebaseapp.com",
+  databaseURL: "https://harshalblogs-52e5f.firebaseio.com",
+  projectId: "harshalblogs-52e5f",
+  storageBucket: "harshalblogs-52e5f.appspot.com",
+  messagingSenderId: "322606791648",
+  appId: "1:322606791648:web:160cf1e23a6c12e7410985",
+  measurementId: "G-WDGT36TCVR"
 };
